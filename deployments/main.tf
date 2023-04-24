bf58be697ae44383055993195cc59bc8976af8b1
suri <alivelusuresh70@gmail.com>
2023-04-24 18:21:58 +0000

    new file

---
+++ deployments/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-02eb7a4783e7e9317" 
+  instance_type = "t2.medium" 
+  key_name = "anji"
+  vpc_security_group_ids= ["sg-081873e1716b7a059"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./anji.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+		}
+  provisioner "remote-exec" {
+	inline = [
+        "sudo apt update -y",
+              "sudo apt install docker.io -y",
+              "sudo snap install microk8s --classic",
+              "sudo sleep 30",
+              #"sudo usermod -aG microk8s $USER",
+             #"sudo chown -f -R $USER ~/.kube",
+             # "sudo microk8s status --wait-ready",
+             # "sudo microk8s enable dns ingress",
+              "sudo microk8s status",
+              "sudo microk8s kubectl create deployment health-deploy --image=554716/newhealthcare:latest",
+              "sudo microk8s kubectl expose deployment health-deploy --port=8082 --type=NodePort",
+              "sudo microk8s kubectl get svc",
+              "sudo echo Public IP Address of the Instance",
+              "sudo curl http://checkip.amazonaws.com",
+  ]
+  } 
+}
---
+++ deployments/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIASLIZDQS3W4NJVBXH"
+  secret_key = "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"
+}
